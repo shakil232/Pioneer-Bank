2ff8f3d073d45d0e654dacbe340306bb1debd40f
unknown <dxshakil2@gmail.com>
2021-01-31 09:06:28 -0800

    done

--- js/pioneer-login.js
+++ js/pioneer-login.js
@@ -7,15 +7,39 @@ let loginButton = document.getElementById('login').addEventListener('click', fun
 })
 // Deposit eventHandler
 let depositButton = document.getElementById('depositButton').addEventListener('click', function(){
-   let depositAmount =document.getElementById('depositAmount').value
-   let depositNumber = parseFloat(depositAmount)
+   // let depositAmount =document.getElementById('depositAmount').value
+   // let depositNumber = parseFloat(depositAmount)
+   let depositNumber = inputNumber('depositAmount')
 
-   let curentDeposit = document.getElementById('curentAmount').innerText;
-   let curentNumber = parseFloat(curentDeposit);
-   let totalDepusit = depositNumber + curentNumber;
-   
-   document.getElementById('curentAmount').innerText = totalDepusit;
-   document.getElementById('depositAmount').value = '';
+   updateSpanText('currentBlance', depositNumber);
+   updateSpanText('curentAmount', depositNumber);
 
-
+  document.getElementById('depositAmount').value = '';
 })
+
+// WithdrawButton eventHandler
+let WithdrawButton = document.getElementById('withdrawButton').addEventListener('click', function(){
+   withdrawNumber =  inputNumber('withdrawAmount');
+   updateSpanText('correntWithdraw',withdrawNumber)
+   updateSpanText('currentBlance', -1* withdrawNumber);
+
+   document.getElementById('withdrawAmount').value = '';
+})
+
+function inputNumber (id){
+   const Amount = document.getElementById(id).value;
+   const AmountNumber = parseFloat(Amount);
+   return AmountNumber;
+}
+
+// updateSpanText
+function updateSpanText(id,depositNumber){
+   let current = document.getElementById(id).innerText;
+   let currentNumber = parseFloat(current);
+   let totalAmount = depositNumber + currentNumber;   
+   document.getElementById(id).innerText = totalAmount;
+
+
+}
+
+  
